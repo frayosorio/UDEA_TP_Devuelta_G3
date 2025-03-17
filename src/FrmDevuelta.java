ece1f20d45bbbf29b02f0a0d2c452a0dc9e278e4
fray osorio <frayosorio@gmail.com>
2025-03-17 15:36:47 -0500

    Se inicia la codificacion de la interfaz grafica

---
+++ src/FrmDevuelta.java
@@ -0,0 +1,30 @@
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.WindowConstants;
+
+public class FrmDevuelta extends JFrame {
+
+    private JComboBox cmbDenominacion;
+    private int[] denominaciones = new int[] { 100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50 };
+
+    public FrmDevuelta() {
+        setSize(400, 400);
+        setTitle("Calculo Devuelta");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(null);
+
+        JLabel lblDenominacion=new JLabel("Denominación");
+        lblDenominacion.setBounds(10,10,100,25);
+        getContentPane().add(lblDenominacion);
+
+        cmbDenominacion = new JComboBox();
+        for (int denominacion : denominaciones) {
+            cmbDenominacion.addItem(denominacion);
+        }
+        cmbDenominacion.setBounds(110,10,100,25);
+        getContentPane().add(cmbDenominacion);
+
+    }
+
+}
